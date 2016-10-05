02353c020cae9ce794ff201104f2314d7945cea3
Or Bin <orbin50@gmail.com>
2016-10-06 00:53:40 +0300

    Added setup-related files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+# http://peterdowns.com/posts/first-time-with-pypi.html
+
+setup(
+  name = 'gitini',
+  packages = ['gitini'],
+  version = '0.1',
+  description = 'The missing Git repository initialization tool',
+  author = 'Sarthak Munshi',
+  author_email = 'sarthakmunshi@gmail.com',
+  url = 'https://github.com/saru95/gitini',
+  download_url = 'https://github.com/saru95/gitini/tarball/0.1',
+  keywords = ['git', 'tool', 'repository'],
+  classifiers = [],
+)
